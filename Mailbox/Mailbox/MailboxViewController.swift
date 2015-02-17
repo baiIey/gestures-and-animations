a51af85a2941ceef3ee9d4d9d016bac60da514dd
Brian Bailey <brian@bailey.is>
2015-02-17 15:15:44 -0800

    Build feed and scrollview

---
+++ Mailbox/Mailbox/MailboxViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MailboxViewController.swift
+//  Mailbox
+//
+//  Created by Brian Bailey on 2/17/15.
+//  Copyright (c) 2015 nevver. All rights reserved.
+//
+
+import UIKit
+
+class MailboxViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        scrollView.contentSize = CGSize(width: 320, height: 1280)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mailbox/Mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mailbox
+//
+//  Created by Brian Bailey on 2/17/15.
+//  Copyright (c) 2015 nevver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
